c91b2964a36b1716bd8249b6bd6249edd1c67c1f
Shelly Tang <shellytang@gmail.com>
2017-05-04 18:50:38 -0700

    tests passing

--- lab-shelly/test/server-test.js
+++ lab-shelly/test/server-test.js
@@ -91,9 +91,68 @@ describe('Server module', () => {
       .post('/api/cat')
       .send({name: 'binky', mood: 'grumpy'})
       .end((err, res) => {
-        // console.log('whats the res body', res.body);
         resource = res.body;
-        console.log('where is it:', resource);
+        done();
+      });
+    });
+
+    after(done => {
+      chai.request(server)
+      .delete('/api/cat')
+      .query({id: resource._id})
+      .end(() => {
+        console.error();
+        done();
+      });
+    });
+    describe('/api/cat/:id route', () => {
+      describe('a properly formmated request', () => {
+        it('should return a 200 response', done => {
+          chai.request(server)
+          .get(`/api/cat/${resource._id}`)
+          .end((err, res) => {
+            expect(res.status).to.equal(200);
+            done();
+          });
+        });
+      });
+      it('should return an item with name: "binky"', done => {
+        chai.request(server)
+        .get(`/api/cat/${resource._id}`)
+        .end((err, res) => {
+          expect(res.body.name).to.equal('binky');
+          done();
+        });
+      });
+      it('should return an item with mood:"grumpy"', done => {
+        chai.request(server)
+        .get(`/api/cat/${resource._id}`)
+        .end((err, res) => {
+          expect(res.body.mood).to.equal('grumpy');
+          done();
+        });
+      });
+    });
+    describe('an unregistered request', () => {
+      it('should return a 404 error', done => {
+        chai.request(server)
+        .get(`/api/dog}`)
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+  });
+// =======PUT =====
+  describe('PUT method', () => {
+    let resource;
+    before(done => {
+      chai.request(server)
+      .post('/api/cat')
+      .send({name: 'binky', mood: 'grumpy'})
+      .end((err, res) => {
+        resource = res.body;
         done();
       });
     });
@@ -109,38 +168,100 @@ describe('Server module', () => {
     });
 
     describe('/api/cat/:id route', () => {
-
-      describe('a properly formmated request', () => {
-        it.only('should return a 200 response', done => {
-          console.log('id: ', resource);
+      describe('a properly formatted request', () => {
+        it('should return a 200 response', done => {
           chai.request(server)
-          .get(`/api/cat/${resource._id}`)
+          .put(`/api/cat/${resource._id}`)
+          .send({name: 'milo', mood: 'happy'})
+          .end((req, res) => {
+            expect(res).to.have.status(200);
+            done();
+          });
+        });
+        it('should return a response object', done => {
+          chai.request(server)
+          .put(`/api/cat/${resource._id}`)
+          .send({name: 'milo', mood: 'happy'})
+          .end((req, res) => {
+            expect(res).to.be.an('object');
+            done();
+          });
+        });
+        it('should update the name to "milo"', done => {
+          chai.request(server)
+          .put(`/api/cat/${resource._id}`)
+          .send({name: 'milo', mood: 'happy'})
           .end((err, res) => {
-            console.log('what is this', res.body);
-            expect(res.status).to.equal(200);
+            expect(res.body.name).to.equal('milo');
+            done();
+          });
+        });
+        it('should update the mood to "happy"', done => {
+          chai.request(server)
+          .put(`/api/cat/${resource._id}`)
+          .send({name: 'milo', mood: 'happy'})
+          .end((err, res) => {
+            expect(res.body.mood).to.equal('happy');
+            done();
+          });
+        });
+      });
+      describe('an invalid request', () => {
+        it('should return a 404 error', done => {
+          chai.request(server)
+          .put(`/api/cat/dog`)
+          .end((err, res) => {
+            expect(res).to.have.status(404);
             done();
           });
         });
       });
     });
-
-
-  });
-// ============
-
-  describe('PUT method', () => {
-
-
-
   });
 // ============
   describe('DELETE method', () => {
+    let resource;
+    before(done => {
+      chai.request(server)
+      .post('/api/cat')
+      .send({name: 'binky', mood: 'grumpy'})
+      .end((err, res) => {
+        resource = res.body;
+        done();
+      });
+    });
+    after(done => {
+      chai.request(server)
+      .delete('/api/cat')
+      .query({id: resource._id})
+      .end(() => {
+        console.error();
+        done();
+      });
+    });
 
+    describe('/api/cat/:id route', () => {
 
-
+      describe('a properly formmated request', () => {
+        it('should return a 204 response', done => {
+          chai.request(server)
+          .delete(`/api/cat/${resource._id}`)
+          .end((err, res) => {
+            expect(res).to.have.status(204);
+            done();
+          });
+        });
+      });
+      describe('a bad request', () => {
+        it('should return a 404 response', done => {
+          chai.request(server)
+          .delete('/api/dog')
+          .end((err, res) => {
+            expect(res).to.have.status(404);
+            done();
+          });
+        });
+      });
+    });
   });
-
-
-
-
 });
